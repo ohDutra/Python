79d5684f42c6d569966937d0729777f65fc84850
ohDutra <112774807+ohDutra@users.noreply.github.com>
2023-09-26 17:17:08 -0300

    aula 4

---
+++ Aulas/aula4.py
@@ -0,0 +1,30 @@
+adicao = 10 + 10
+print(adicao)
+
+subtracao = 10 - 5
+print(subtracao)
+
+
+multiplicacao = 10  * 10
+print(multiplicacao)
+
+divisao = 10 / 2.2
+print(divisao)
+
+divisao_inteira = 10 // 3 
+print(divisao_inteira)
+
+exponenciacao = 2 ** 10
+print(exponenciacao)
+
+modulo = 25% 5
+print(modulo)
+
+
+
+concatenacao = 'Gabriel' + 'Dutra'
+print(concatenacao)
+
+
+repeticao = 'A' * 10
+print(repeticao)
