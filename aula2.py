53eba3ccaabcb0f75a9c8a655965ddaf93579bdc
ohDutra <112774807+ohDutra@users.noreply.github.com>
2023-09-25 22:50:55 -0300

    Upp aulas

--- aula2.py
+++ aula2.py
@@ -4,3 +4,17 @@ print(1234)
 print('Gabriel Dutra')
 #aspas duplas
 print("Gabriel Dutra")
+
+
+
+print(  type('Gabriel')  )
+print(  type(123)  )
+
+#Sim (True) ou não (false)
+print(10 == 10)  
+print(10 == 11)  
+
+print(int('1'), type(int('1')) )
+print(type(float('1')+ 1))
+print(bool(' '))
+print(str(11) + 'g')
---
+++ áula3.py
@@ -0,0 +1,7 @@
+nome = "Gabriel Dutra"
+idade = 25
+MaiorIdade = idade >= 18 
+
+
+print('Nome :',nome, 'Idade :',idade ,)
+print('É maior?', MaiorIdade)
