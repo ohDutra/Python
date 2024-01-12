7a3e2fd0b7d5d7de4a161d323908fdc5ab88e1b9
ohDutra <112774807+ohDutra@users.noreply.github.com>
2024-01-12 19:56:14 -0300

    Create aula32.py

---
+++ Aulas/aula32.py
@@ -0,0 +1,13 @@
+pessoa={
+    'nome' : 'Gabriel Dutra',
+    'idade' : 26
+
+}
+
+#print(len(pessoa))   
+#print(pessoa.keys())   
+#print(pessoa.values())   
+# print(pessoa.items())   
+
+for chave, valor in pessoa.items():
+    print(chave, valor)
